164050ef0c1d7c2d26d42e2b1e783a08538d8b82
kyleqiq <kyleqiq@gmail.com>
2024-11-17 22:42:02 +0900

    fix: fix favicon and details

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,6 +1,28 @@
 import Logo from "./components/Logo";
 
 export default {
+  head: (
+    <>
+      <title>react2app</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="react2app" />
+      <meta property="og:description" content="React to App in 3 commands" />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+    </>
+  ),
   logo: <Logo />,
   project: {
     link: "https://github.com/kyleqiq/react2app",
