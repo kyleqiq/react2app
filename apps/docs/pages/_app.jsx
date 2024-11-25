8d5fdd8de5a58cce8992af90a0f44f9a8aa48f85
kyleqiq <kyleqiq@gmail.com>
2024-11-25 22:17:46 +0900

    feat: add clarity

--- apps/docs/pages/_app.jsx
+++ apps/docs/pages/_app.jsx
@@ -1,4 +1,7 @@
 import "../assets/globals.css";
+import { Clarity } from "microsoft-clarity";
+
+Clarity.init(process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
