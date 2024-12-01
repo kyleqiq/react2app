87401ab2a69255ad0aa2e89989dc9ffdfc54efc7
kyleqiq <kyleqiq@gmail.com>
2024-12-01 19:10:38 +0900

    refactor: remove unused import

--- apps/react2app/src/cli/commands/init.ts
+++ apps/react2app/src/cli/commands/init.ts
@@ -1,11 +1,4 @@
-import { DEFAULT_DEV_SERVER_PORT } from "../constants/index.js";
-import { FRAMEWORK } from "../constants/index.js";
-import {
-  createExpoEnvFile,
-  createExpoProject,
-  updateExpoEnvFile,
-} from "../utils/expo.js";
-import { getLocalIPAddress } from "../utils/network.js";
+import { createExpoEnvFile, createExpoProject } from "../utils/expo.js";
 import { createR2AConfig } from "../utils/r2aConfig.js";
 
 export const init = async () => {
