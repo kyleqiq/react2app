90d5478462a816d5fc5d656f34820362fcc490a2
kyleqiq <kyleqiq@gmail.com>
2024-12-04 18:57:42 +0900

    fix: fix site title

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -3,7 +3,7 @@ import Logo from "./components/Logo";
 export default {
   head: (
     <>
-      <title>react2app</title>
+      <title>next2app</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="next2app" />
       <meta property="og:description" content="Next.js to App in 3 commands" />
