3d79ad0d93c086c7d3296f98d2ceb2ac57b042e1
kyleqiq <kyleqiq@gmail.com>
2024-12-17 22:43:01 +0900

    fix: fix AppLayout import issue

--- apps/next2app/src/cli/utils/ux.ts
+++ apps/next2app/src/cli/utils/ux.ts
@@ -9,10 +9,13 @@ export const addAppLayout = async () => {
     return;
   }
 
-  const newLayoutContent = layoutContent.replace(
-    /(<body[^>]*>)([\s\S]*?)(\{children\})([\s\S]*?)(<\/body>)/,
-    "$1$2<AppLayout>{children}</AppLayout>$4$5"
-  );
+  const importStatement = "import { AppLayout } from 'next2app';\n";
+  const newLayoutContent =
+    importStatement +
+    layoutContent.replace(
+      /(<body[^>]*>)([\s\S]*?)(\{children\})([\s\S]*?)(<\/body>)/,
+      "$1$2<AppLayout>{children}</AppLayout>$4$5"
+    );
 
   await fs.writeFile(layoutPath, newLayoutContent);
 };
@@ -21,7 +24,12 @@ export const removeAppLayout = async () => {
   const layoutPath = PATHS.NEXTJS.LAYOUT_FILE;
   const layoutContent = await fs.readFile(layoutPath, "utf8");
 
-  const newLayoutContent = layoutContent.replace(
+  const withoutImport = layoutContent.replace(
+    /import\s*{\s*AppLayout\s*}\s*from\s*['"]next2app['"];\s*\n/,
+    ""
+  );
+
+  const newLayoutContent = withoutImport.replace(
     /(<body[^>]*>)([\s\S]*?)<AppLayout>([\s\S]*?)<\/AppLayout>([\s\S]*?)(<\/body>)/,
     "$1$2$3$4$5"
   );
